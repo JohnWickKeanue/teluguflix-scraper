a918ec5237eab46c20421b301098489efa84b6aa
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2022-11-19 12:50:57 +0530

    updated

--- teluguflix.py
+++ teluguflix.py
@@ -2,20 +2,19 @@ import time
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://teluguflix.xyz/sita-ramam-2022-hindi-full-movie-download-dsnp-web-dl-1080p-720p-576p-avc-aac-2-0-esub/"
+url = "https://teluguflix.xyz/aha-naa-pellanta-2022-season01-all-episodes-download-zee5-web-dl-4k-2160p-1080p-720p-480p-hevc-avc-dd5-1-192kbps-esubs/"
 
 def flix(url):
     client = requests.session()
-    r = client.get(url).text
-    y = r.split('id="download"')[-1]
-    f = y.split("</ul>") [0]
-    soup = BeautifulSoup (f, "html.parser")
+    Jack = client.get(url).text
+    soup = BeautifulSoup (Jack, "html.parser")
     for a in soup.find_all("a"):
-              c = a.get("href")
-              t = client.get(c).text
-              soupt = BeautifulSoup(t, "html.parser")
-              title = soupt.title
-              gd_txt = f"{(title.text).replace('GDToT | ' , '')}\n{c}\n\n"
-              print(gd_txt)
+              c = a.get("href") 
+              if "gdtot" in c:
+                     t = client.get(c).text
+                     soupt = BeautifulSoup(t, "html.parser")
+                     title = soupt.title
+                     gd_txt = f"{(title.text).replace('GDToT | ' , '')}\n{c}\n\n"
+                     print(gd_txt) 
 
 print(flix(url))
