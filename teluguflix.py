80fb35bdac9177be316cd845c484ebc04ace6c0e
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2023-02-03 19:08:11 +0530

    Update teluguflix.py

--- teluguflix.py
+++ teluguflix.py
@@ -2,19 +2,19 @@ import time
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://teluguflix.xyz/aha-naa-pellanta-2022-season01-all-episodes-download-zee5-web-dl-4k-2160p-1080p-720p-480p-hevc-avc-dd5-1-192kbps-esubs/"
+url = ""
 
 def flix(url):
     client = requests.session()
-    Jack = client.get(url).text
-    soup = BeautifulSoup (Jack, "html.parser")
-    for a in soup.find_all("a"):
-              c = a.get("href") 
-              if "gdtot" in c:
-                     t = client.get(c).text
-                     soupt = BeautifulSoup(t, "html.parser")
-                     title = soupt.title
-                     gd_txt = f"{(title.text).replace('GDToT | ' , '')}\n{c}\n\n"
-                     print(gd_txt) 
+    r = client.get(url)
+    soup = BeautifulSoup (r.text, "html.parser")
+        links = soup.select('a[href*="gdtot"]')
+        if len(links) == 0:
+             links = soup.select('a[href*="filepress"]')
+        gd_txt = f"Total Links Found : {len(links)}\n\n"
+        sendMessage(gd_txt, context.bot, update.message)
+        for a in links:
+             link = a["href"]
+             print(link) 
 
 print(flix(url))
